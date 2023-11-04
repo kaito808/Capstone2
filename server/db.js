dd03fc157df5c782f783db2d690c69030656d199
Kaito <107011546+kaito808@users.noreply.github.com>
2023-11-04 11:50:00 -1000

    Update db.js

--- server/db.js
+++ server/db.js
@@ -4,6 +4,6 @@ const pgp = require("pg-promise")();
 //   "postgres://postgres:shahzaib2054@localhost:5001/data.sql";
 
 // const db = pgp(connectionString);
-db = pgp(process.env.DATABASE_URL);
+const db = pgp(process.env.DATABASE_URL);
 
 module.exports = { db };
